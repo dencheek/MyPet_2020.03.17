3f03b97138cc85a917ffe146058152e484951186
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 15:46:44 +0300

    Some corrects...

--- app/src/main/java/com/example/mypets/AppDatabase.java
+++ app/src/main/java/com/example/mypets/AppDatabase.java
@@ -1,7 +1,7 @@
 package com.example.mypets;
-
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+
 @Database(entities = {Animal.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AnimalDao animalDao();
