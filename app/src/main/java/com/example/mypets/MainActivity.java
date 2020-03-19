7e1e488cd04564b67f447b2c96890afa08b0d575
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 19:18:43 +0300

    Questions to understanding RecyclerView

--- app/src/main/java/com/example/mypets/MainActivity.java
+++ app/src/main/java/com/example/mypets/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.mypets;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-
+/
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -13,10 +14,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_recycler_view);//activity_main);
+        setContentView(R.layout.my_activity);//activity_main);
         recyclerView = (RecyclerView) findViewById(R.layout.my_recycler_view);
 
         mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(myAdapter);
+        recyclerView.setAdapter(mAdapter);
     }
 }
+/*
+public class MyActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_activity);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+    }
+    // ...
+}
+*/
