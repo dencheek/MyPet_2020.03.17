f0047a0e2050cb1b01e7d06c690fded106120cac
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 16:18:26 +0300

    Added to Animal class: constructor, populateData

--- app/src/main/java/com/example/mypets/Animal.java
+++ app/src/main/java/com/example/mypets/Animal.java
@@ -9,9 +9,24 @@ public class Animal {
     @PrimaryKey private int id;
     @ColumnInfo(name = "Animal") @NonNull private String Animal;
     @ColumnInfo(name = "Name") @NonNull private String Name;
-    public int getId() {return id;}
-    public String getAnimal() {return Animal;}
-    public void setAnimal(String Animal) {this.Animal = Animal;}
-    public String getName() {return Name;}
-    public void setName(String Name) {this.Name = Name;}
+
+    public Animal (String Animal, String Name) {
+        this.Animal = Animal;
+        this.Name = Name;
+    }
+    public int      getId()                     {return id;}
+    public String   getAnimal()                 {return Animal;}
+    public String   getName()                   {return Name;}
+    public void     setAnimal(String Animal)    {this.Animal = Animal;}
+    public void     setName(String Name)        {this.Name = Name;}
+
+    public static Animal[] populateData() {
+        return new Animal[] {
+                 new Animal ("Кот","Василий")
+                ,new Animal ("Собака","Барбос")
+                ,new Animal ("Лиса","Алиса")
+                ,new Animal ("Черепаха","Вениамин")
+                ,new Animal ("Крокодил","Гена")
+        };
+    }
 }
