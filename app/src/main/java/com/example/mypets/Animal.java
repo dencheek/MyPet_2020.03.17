9ca542651e460fd5bc7326a913663f80b940cd3a
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 13:18:46 +0300

    Added getters&setters to class Animal

--- app/src/main/java/com/example/mypets/Animal.java
+++ app/src/main/java/com/example/mypets/Animal.java
@@ -8,4 +8,9 @@ public class Animal {
     @PrimaryKey private int id;
     @ColumnInfo(name = "Animal") private String Animal;
     @ColumnInfo(name = "Name") private String Name;
+    public int getId() {return id;}
+    public String getAnimal() {return Animal;}
+    public void setAnimal(String Animal) {this.Animal = Animal;}
+    public String getName() {return Name;}
+    public void setName(String Name) {this.Name = Name;}
 }
