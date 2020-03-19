81e5a882f258c7cd85197bc028e2e534270a8bf1
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 17:02:03 +0300

    Changed in Animal class whitespaces on tab in column definitions_2

--- app/src/main/java/com/example/mypets/Animal.java
+++ app/src/main/java/com/example/mypets/Animal.java
@@ -6,19 +6,19 @@ import androidx.room.PrimaryKey;
 
 @Entity
 public class Animal {
-    @PrimaryKey private int id;
+    @PrimaryKey                           private int    id;
     @ColumnInfo(name = "Animal") @NonNull private String Animal;
     @ColumnInfo(name = "Name")   @NonNull private String Name;
 
     public Animal (String Animal, String Name) {
         this.Animal = Animal;
-        this.Name = Name;
+        this.Name   = Name;
     }
     public int      getId()                     {return id;}
     public String   getAnimal()                 {return Animal;}
     public String   getName()                   {return Name;}
     public void     setAnimal(String Animal)    {this.Animal = Animal;}
-    public void     setName(String Name)        {this.Name = Name;}
+    public void     setName  (String Name)      {this.Name   = Name;}
 
     public static Animal[] populateData() {
         return new Animal[] {
