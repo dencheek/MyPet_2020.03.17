df1385e9f7a96e243d202caa2ce9e444eb552fcc
kvs <kuzmenko.vladislav@gmail.com>
2020-03-19 18:03:02 +0300

    Changed activity_main.xml by adding support RecyclerView

--- app/src/main/java/com/example/mypets/MainActivity.java
+++ app/src/main/java/com/example/mypets/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.mypets;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
